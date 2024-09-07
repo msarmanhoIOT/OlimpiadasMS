d4e11ece12e8943ad87899137b41e95cc6769d78
msarmanhoIOT <mjssarmanho@gmail.com>
2024-09-06 21:16:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+function pesquisar() {
+
+    // Obtém a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    
+ 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // Se o campo pesquisa for uma String sem Nada
+    if (!campoPesquisa) {
+        section.innerHTML ="<p>NADA FOI ENCONTRADO VOCE PRECISA DIGITAR O NOME DO ATLETA OU ESPORTE</p>"
+        return
+    }
+    
+    campoPesquisa = campoPesquisa.toLowerCase ()
+
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    // Itera sobre cada objeto 'dado' dentro do array 'dados'
+    for (let dado of dados) {
+     titulo = dado.titulo.toLowerCase()
+     descricao = dado.descricao.toLowerCase()
+     tags = dado.tags.toLowerCase()
+
+        // Se o titulo includes campo pesquisa 
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) ) {
+      // Cria um novo elemento 
+        resultados += `
+        <div class="item-resultado">
+            <h2> 
+                <a href="https://pt.wikipedia.org/wiki/Rayssa_Leal" target="_blank">${dado.titulo}</a> 
+            </h2>
+            <p class="descricao-meta">
+                ${dado.descricao} 
+            </p>
+            <a href=${dado.link} target="_blank">Mais Informações</a>
+        </div>
+    `; 
+      }
+              
+    }
+
+    if (!resultados) {
+        resultados = "<p> Nada foi Encontrado </p>"
+    }
+    console.log(campoPesquisa);
+    // Define o conteúdo HTML da seção com os resultados formatados
+    section.innerHTML = resultados;
+}
+// console.log(dados);
+
---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+    titulo : "Rayssa Leal",
+    descricao : "Rayssa Leal é uma skatista brasileira, conhecida como Fadinha. Conquistou a medalha de prata nos Jogos Olímpicos de Tóquio/2020 e o Bronze em Paris/2024 no skate street, sendo a mais jovem medalhista brasileira da história.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags : "skate fada sk8"
+},
+{
+    titulo : "Rebeca Andrade",
+    descricao : "Rebeca Andrade é uma ginasta brasileira. Conquistou a medalha de ouro no salto e a medalha de prata no individual geral nos Jogos Olímpicos de Tóquio/2020, sendo a primeira ginasta brasileira a conquistar uma medalha olímpica.",
+    link : "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags : "ginastica ginástica solo olimpica"
+},
+{
+    titulo : "Beatriz Souza",
+    descricao : "Beatriz Souza é uma judoca brasileira. Conquistou a medalha de prata na categoria +78kg nos Jogos Olímpicos de Tóquio/2020.",
+    link : "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags : "judo judô judoka pesado"
+}
+];
+    
